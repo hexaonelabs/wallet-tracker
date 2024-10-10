f83fc9a2e1e1e01c6e6d7f366c885d9d9021134f
FazioNico <contact@nicolasfazio.ch>
2024-10-10 14:32:10 +0200

    refactor: add limit in DevMode

--- src/app/services/db/db.service.ts
+++ src/app/services/db/db.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import {
   collection,
   Firestore,
@@ -9,9 +9,11 @@ import {
   addDoc,
   doc,
   deleteDoc,
+  QueryConstraint,
 } from '@angular/fire/firestore';
 import { BehaviorSubject } from 'rxjs';
 import { Tx, UserWallet } from '../../interfaces';
+import { limit } from 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root',
@@ -20,24 +22,29 @@ export class DBService {
   private readonly _COLLECTIONS = {
     tx: 'txs',
     userWallet: 'user-wallet',
+    defiProtocols: 'defi-protocols',
   };
   private readonly _txs$: BehaviorSubject<Tx[]> = new BehaviorSubject<Tx[]>(
-    undefined as unknown as Tx[]
+    [] as unknown as Tx[]
   );
   private readonly _userWallets$: BehaviorSubject<UserWallet[]> =
     new BehaviorSubject<UserWallet[]>([]);
+  private readonly _defiProtocols$: BehaviorSubject<any[]> =
+    new BehaviorSubject([] as unknown as Tx[]);
   private readonly _subscriptions: {
     sub: Subscribtion;
     collection: string;
   }[] = [];
   public readonly txs$ = this._txs$.asObservable();
   public readonly userWallets$ = this._userWallets$.asObservable();
+  public readonly defiProtocols$ = this._defiProtocols$.asObservable();
 
   constructor(private readonly _firestore: Firestore) {}
 
   async loadUserData(uid: string) {
     this._loadTxs(uid);
     this._loadUserWallets(uid);
+    this._loadDefiProtocols(uid);
   }
 
   async clearData() {
@@ -45,6 +52,7 @@ export class DBService {
     this._subscriptions.length = 0;
     this._txs$.next([]);
     this._userWallets$.next([]);
+    this._defiProtocols$.next([]);
   }
 
   async addTx(tx: Omit<Tx, 'id'>) {
@@ -65,7 +73,10 @@ export class DBService {
       return;
     }
     const colRef = collection(this._firestore, this._COLLECTIONS.tx);
-    const constraints = [where('uid', '==', uid)];
+    const constraints = [
+      where('uid', '==', uid),
+      isDevMode() ? limit(5) : null,
+    ].filter(Boolean) as QueryConstraint[];
     const q = query(colRef, ...constraints);
     const sub = onSnapshot(q, (snapshot) => {
       const data = snapshot
@@ -126,5 +137,29 @@ export class DBService {
       this._userWallets$.next(data);
     });
     this._subscriptions.push({ collection: this._COLLECTIONS.userWallet, sub });
+  }
+
+  private async _loadDefiProtocols(uid: string) {
+    // check if there is already a subscription
+    if (
+      this._subscriptions.find(
+        (sub) => sub.collection === this._COLLECTIONS.defiProtocols
+      )
+    ) {
+      return;
+    }
+    const colRef = collection(this._firestore, this._COLLECTIONS.defiProtocols);
+    const constraints: QueryConstraint[] = [
+      // where('uid', '==', uid)
+    ];
+    const q = query(colRef, ...constraints);
+    const sub = onSnapshot(q, (snapshot) => {
+      const data = snapshot.docs.map((doc) => doc.data() as any);
+      this._defiProtocols$.next(data);
+    });
+    this._subscriptions.push({
+      collection: this._COLLECTIONS.defiProtocols,
+      sub,
+    });
   }
 }
