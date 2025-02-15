59a70a448256c50cf5ad9825ac49f195722bf5d4
FazioNico <contact@nicolasfazio.ch>
2025-02-15 09:34:30 +0100

    refactor: add hype as manual id

--- src/app/app.utils.ts
+++ src/app/app.utils.ts
@@ -48,6 +48,7 @@ export const addMarketDatas = async <T>(
     { tickerId: 'jup', apiId: 'jupiter-exchange-solana' },
     { tickerId: 'velo', apiId: 'velodrome-finance' },
     { tickerId: 'op', apiId: 'optimism' },
+    { tickerId: 'hype', apiId: 'hyperliquid' },
   ];
   const tickerIds = assetPositions.map((asset) => asset.tickerId);
   const coinsList = await _coinsService.getAllCoinsId();
